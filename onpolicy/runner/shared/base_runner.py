f6566e1c3e92d46aad93aabc6a3084cd7ff43c29
sapios <30330-sapios@users.noreply.gitlab.aicrowd.com>
2022-07-07 08:31:07 +0000

    fix runner param

--- onpolicy/runner/shared/base_runner.py
+++ onpolicy/runner/shared/base_runner.py
@@ -137,8 +137,8 @@ class Runner(object):
                                                         np.concatenate(self.buffer.masks[-1]))
         else:
             next_values = self.trainer.policy.get_values(np.concatenate(self.buffer.share_obs[-1]),
-                                                        np.concatenate(self.buffer.obs[-1]),
-                                                        np.concatenate(self.buffer.rnn_states_critic[-1]))
+                                                        np.concatenate(self.buffer.rnn_states_critic[-1]),
+                                                        np.concatenate(self.buffer.masks[-1]))
         next_values = np.array(np.split(_t2n(next_values), self.n_rollout_threads))
         self.buffer.compute_returns(next_values, self.trainer.value_normalizer)
     
--- onpolicy/scripts/train_football.sh
+++ onpolicy/scripts/train_football.sh
@@ -6,8 +6,9 @@ scenario="academy_counterattack_easy"
 # academy_counterattack_easy
 n_agent=4
 algo="mappo"
-exp="single"
+exp="retrain"
 seed=1
 
 echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, exp is ${exp}, seed is ${seed}"
-CUDA_VISIBLE_DEVICES=5 python train/train_football.py --seed ${seed} --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario ${scenario} --n_agent ${n_agent} --lr 5e-4 --entropy_coef 0.01 --max_grad_norm 0.5 --eval_episodes 32 --n_training_threads 16 --n_rollout_threads 20 --num_mini_batch 1 --episode_length 200 --eval_interval 25 --num_env_steps 10000000 --ppo_epoch 10 --clip_param 0.05 --use_eval --use_value_active_masks --use_policy_active_masks --use_recurrent_policy False
+CUDA_VISIBLE_DEVICES=5 python train/train_football.py --seed ${seed} --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario ${scenario} --n_agent ${n_agent} --lr 5e-4 --entropy_coef 0.01 --max_grad_norm 0.5 --eval_episodes 32 --n_training_threads 16 --n_rollout_threads 20 --num_mini_batch 1 --episode_length 200 --eval_interval 25 --num_env_steps 10000000 \
+--ppo_epoch 5 --clip_param 0.2 --layer_N 2 --use_eval --use_value_active_masks --use_policy_active_masks --use_recurrent_policy False
