559bf100bf2b47fc47ec61c233beb4e4e2243348
sapios <30330-sapios@users.noreply.gitlab.aicrowd.com>
2022-08-11 11:26:49 +0000

    running setting

--- onpolicy/scripts/train_football.sh
+++ onpolicy/scripts/train_football.sh
@@ -12,31 +12,31 @@ seed_max=3
 # scenario="academy_3_vs_1_with_keeper"
 # n_agent=3
 
-scenario="academy_counterattack_easy"
-n_agent=4
+# scenario="academy_counterattack_easy"
+# n_agent=4
 
 # scenario="academy_counterattack_hard"
 # n_agent=4
 
-# scenario="academy_single_goal_versus_lazy"
-# n_agent=11
+scenario="academy_single_goal_versus_lazy"
+n_agent=11
 
 for seed in `seq ${seed_max}`;
 do
     echo "seed is ${seed}:"
     algo="rmappo"
     CUDA_VISIBLE_DEVICES=0 python train/train_football.py --seed ${seed} --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario ${scenario} --n_agent ${n_agent} \
-    --num_env_steps 20000000 --n_rollout_threads 50 --ppo_epoch 15 --episode_length 200 --num_mini_batch 2 --data_chunk_length 10 --save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --eval_episodes 100 --n_eval_rollout_threads 100
+    --num_env_steps 25000000 --n_rollout_threads 50 --ppo_epoch 15 --episode_length 200 --num_mini_batch 2 --data_chunk_length 10 --use_eval --eval_episodes 100 --n_eval_rollout_threads 100
     #--lr 5e-4 --entropy_coef 0.01 --max_grad_norm 0.5 --eval_episodes 32 --n_training_threads 16 --n_rollout_threads 20 --num_mini_batch 1 --episode_length 200 --eval_interval 100 --num_env_steps 10000000 --ppo_epoch 5 --clip_param 0.2 --layer_N 2 --use_eval --use_value_active_masks --use_policy_active_masks
     
     algo="mat"
     CUDA_VISIBLE_DEVICES=0 python train/train_football.py --seed ${seed} --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario ${scenario} --n_agent ${n_agent} \
-    --num_env_steps 20000000 --n_rollout_threads 50 --ppo_epoch 15 --episode_length 200 --num_mini_batch 1 --save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --eval_episodes 100 --n_eval_rollout_threads 100 --ppo_epoch 10 --clip_param 0.05 --use_eval --use_value_active_masks --use_policy_active_masks --use_recurrent_policy
+    --num_env_steps 25000000 --n_rollout_threads 50 --ppo_epoch 15 --episode_length 200 --num_mini_batch 1 --use_eval --eval_episodes 100 --n_eval_rollout_threads 100 --ppo_epoch 10 --clip_param 0.05 --use_eval --use_value_active_masks --use_policy_active_masks --use_recurrent_policy
     # --lr 5e-4 --entropy_coef 0.01 --max_grad_norm 0.5 --eval_episodes 32 --n_training_threads 16 --n_rollout_threads 20 --num_mini_batch 1 --episode_length 200 --eval_interval 25 --num_env_steps 10000000 --ppo_epoch 10 --clip_param 0.05 --use_eval --use_value_active_masks --use_policy_active_masks --use_recurrent_policy
 
     algo="mat_dec"
     CUDA_VISIBLE_DEVICES=0 python train/train_football.py --seed ${seed} --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --scenario ${scenario} --n_agent ${n_agent} \
-    --num_env_steps 20000000 --n_rollout_threads 50 --ppo_epoch 15 --episode_length 200 --num_mini_batch 1 --save_interval 200000 --log_interval 200000 --use_eval --eval_interval 400000 --eval_episodes 100 --n_eval_rollout_threads 100 --ppo_epoch 10 --clip_param 0.05 --use_eval --use_value_active_masks --use_policy_active_masks --use_recurrent_policy
+    --num_env_steps 25000000 --n_rollout_threads 50 --ppo_epoch 15 --episode_length 200 --num_mini_batch 1 --use_eval --eval_episodes 100 --n_eval_rollout_threads 100 --ppo_epoch 10 --clip_param 0.05 --use_eval --use_value_active_masks --use_policy_active_masks --use_recurrent_policy
     # --lr 5e-4 --entropy_coef 0.01 --max_grad_norm 0.5 --eval_episodes 32 --n_training_threads 16 --n_rollout_threads 20 --num_mini_batch 1 --episode_length 200 --eval_interval 25 --num_env_steps 10000000 --ppo_epoch 10 --clip_param 0.05 --use_eval --use_value_active_masks --use_policy_active_masks --use_recurrent_policy
 
 done
