5554b4d60c6ee95b462572dbfc96bb0e6da785b7
xieyuqing22 <xieyuqing22@sandbox.eoe2.nics.info>
2022-08-15 07:28:26 +0000

    smac param

--- onpolicy/scripts/train_smac.sh
+++ onpolicy/scripts/train_smac.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
 env="StarCraft2"
 map="MMM2"
-algo="rmappo"
-exp="0808"
-seed_max=5
+exp="0815"
+seed_max=1
 
-echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
-for seed in `seq ${seed_max}`;
+echo "env is ${env}, map is ${map}, exp is ${exp}, max seed is ${seed_max}"
+for seed in {4..6..1}
 do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=4 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} \
-    --n_training_threads 1 --n_rollout_threads 8 --num_mini_batch 2 --episode_length 100 --num_env_steps 10000000 --ppo_epoch 5 --use_value_active_masks --use_eval --add_center_xy --use_state_agent --gain 1
-    #--model_dir /home/eva_data/xyq/on-policy/onpolicy/scripts/results/StarCraft2/6h_vs_8z/mat/retrain/wandb/run-20220706_172156-3h1iua8u/files/transformer_3124.pt --use_wandb False -- use_obs_instead_of_state True
-done
 
-# mappo: --ppo_epoch 5 --clip_param 0.2 \
-# mat: --ppo_epoch 15 --clip_param 0.05 \
-d
-# mappo
-#/home/eva_data/xyq/on-policy/onpolicy/scripts/results/StarCraft2/6h_vs_8z/mappo/retrain/wandb/run-20220706_180611-1qogpf0r
-# mat
-#/home/eva_data/xyq/on-policy/onpolicy/scripts/results/StarCraft2/6h_vs_8z/mat/retrain/wandb/run-20220706_172156-3h1iua8u/files/transformer_3124.pt
-# mat_dec
-#/home/eva_data/xyq/on-policy/onpolicy/scripts/results/StarCraft2/6h_vs_8z/mat_dec/retrain/wandb/run-20220706_172139-2edigroz/files/transformer_3124.pt
+    algo="mat"
+    echo "algo is ${algo}"
+    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} \
+    --n_training_threads 1 --n_rollout_threads 8 --episode_length 400 --num_env_steps 10000000 \
+    --num_mini_batch 1 --ppo_epoch 5 --clip_param 0.05 --use_value_active_masks --use_eval --use_recurrent_policy
+
+    algo="mat_dec"
+    echo "algo is ${algo}"
+    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} \
+    --n_training_threads 1 --n_rollout_threads 8 --episode_length 400 --num_env_steps 10000000 \
+    --num_mini_batch 1 --ppo_epoch 5 --clip_param 0.05 --use_value_active_masks --use_eval --use_recurrent_policy
+
+    algo="rmappo"
+    echo "algo is ${algo}"
+    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} \
+    --n_training_threads 1 --n_rollout_threads 8 --episode_length 400 --num_env_steps 10000000 \
+    --num_mini_batch 2 --ppo_epoch 5 --gain 1 --use_value_active_masks --use_eval #--use_recurrent_policy
+
+done
