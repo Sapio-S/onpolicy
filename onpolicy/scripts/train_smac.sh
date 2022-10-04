86fa9c6575de17e422cebcd434628fe12f8b523f
xieyuqing22 <xieyuqing22@sandbox.eoe2.nics.info>
2022-10-04 09:44:10 +0000

    store for comparison

--- onpolicy/scripts/train_smac.sh
+++ onpolicy/scripts/train_smac.sh
@@ -28,7 +28,7 @@
 
 #!/bin/sh
 env="StarCraft2"
-map="27m_vs_30m"
+map="3m"
 exp="0826"
 seed_max=1
 
@@ -42,14 +42,14 @@ do
     # CUDA_VISIBLE_DEVICES=0 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} \
     # --n_training_threads 1 --n_rollout_threads 8 --episode_length 400 --num_env_steps 10000000 --num_mini_batch 1 --ppo_epoch 5 --clip_param 0.2 --use_value_active_masks --use_eval --use_recurrent_policy
 
-    algo="mat_dec"
-    echo "algo is ${algo}"
-    CUDA_VISIBLE_DEVICES=0 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} \
-    --n_training_threads 1 --n_rollout_threads 8 --episode_length 400 --num_env_steps 10000000 --num_mini_batch 1 --ppo_epoch 5 --clip_param 0.2 --use_value_active_masks --use_eval --use_recurrent_policy
-    
-    # algo="rmappo"
+    # algo="mat_dec"
     # echo "algo is ${algo}"
-    # CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} \
-    # --n_training_threads 1 --n_rollout_threads 8 --episode_length 400 --num_env_steps 5000000 --num_mini_batch 1 --ppo_epoch 5 --clip_param 0.2 --gain 0.01 --use_value_active_masks --use_eval
+    # CUDA_VISIBLE_DEVICES=0 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} \
+    # --n_training_threads 1 --n_rollout_threads 8 --episode_length 400 --num_env_steps 10000000 --num_mini_batch 1 --ppo_epoch 5 --clip_param 0.2 --use_value_active_masks --use_eval --use_recurrent_policy
+    
+    algo="rmappo"
+    echo "algo is ${algo}"
+    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} \
+    --n_training_threads 1 --n_rollout_threads 2 --episode_length 400 --num_env_steps 5000000 --num_mini_batch 1 --ppo_epoch 5 --clip_param 0.2 --gain 0.01 --use_value_active_masks --use_eval --use_wandb
 
 done
